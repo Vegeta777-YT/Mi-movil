2858c5eba7609cb83334a7b1903982931080f1df
Vegeta777-YT <152323085+Vegeta777-YT@users.noreply.github.com>
2023-11-28 19:36:42 +0100

    Add files via upload

---
+++ Dame mi movil chaquetón.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damemimovilchaquetón : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Helloword");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
